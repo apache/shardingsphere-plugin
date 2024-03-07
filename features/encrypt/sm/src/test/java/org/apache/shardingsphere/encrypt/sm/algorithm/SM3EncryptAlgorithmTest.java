b12f98c05871b8f920f9ee73017c0e1a83e7aa5c
张忠源 <zzypersonally@gmail.com>
2024-03-07 16:49:07 +0800

    fix :Class ‘EncryptContext’ has remove

--- features/encrypt/sm/src/test/java/org/apache/shardingsphere/encrypt/sm/algorithm/SM3EncryptAlgorithmTest.java
+++ features/encrypt/sm/src/test/java/org/apache/shardingsphere/encrypt/sm/algorithm/SM3EncryptAlgorithmTest.java
@@ -17,8 +17,8 @@
 
 package org.apache.shardingsphere.encrypt.sm.algorithm;
 
-import org.apache.shardingsphere.encrypt.api.context.EncryptContext;
 import org.apache.shardingsphere.encrypt.spi.EncryptAlgorithm;
+import org.apache.shardingsphere.infra.algorithm.core.context.AlgorithmSQLContext;
 import org.apache.shardingsphere.infra.spi.type.typed.TypedSPILoader;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -43,36 +43,36 @@ class SM3EncryptAlgorithmTest {
     
     @Test
     void assertEncrypt() {
-        Object actual = encryptAlgorithm.encrypt("test1234", mock(EncryptContext.class));
+        Object actual = encryptAlgorithm.encrypt("test1234", mock(AlgorithmSQLContext.class));
         assertThat(actual, is("9587fe084ee4b53fe629c6ae5519ee4d55def8ed4badc8588d3be9b99bd84aba"));
     }
     
     @Test
     void assertEncryptWithoutSalt() {
         encryptAlgorithm.init(new Properties());
-        assertThat(encryptAlgorithm.encrypt("test1234", mock(EncryptContext.class)), is("ab847c6f2f6a53be88808c5221bd6ee0762e1af1def82b21d2061599b6cf5c79"));
+        assertThat(encryptAlgorithm.encrypt("test1234", mock(AlgorithmSQLContext.class)), is("ab847c6f2f6a53be88808c5221bd6ee0762e1af1def82b21d2061599b6cf5c79"));
     }
     
     @Test
     void assertEncryptWithNullPlaintext() {
-        assertNull(encryptAlgorithm.encrypt(null, mock(EncryptContext.class)));
+        assertNull(encryptAlgorithm.encrypt(null, mock(AlgorithmSQLContext.class)));
     }
     
     @Test
     void assertDecrypt() {
-        Object actual = encryptAlgorithm.decrypt("ab847c6f2f6a53be88808c5221bd6ee0762e1af1def82b21d2061599b6cf5c79", mock(EncryptContext.class));
+        Object actual = encryptAlgorithm.decrypt("ab847c6f2f6a53be88808c5221bd6ee0762e1af1def82b21d2061599b6cf5c79", mock(AlgorithmSQLContext.class));
         assertThat(actual.toString(), is("ab847c6f2f6a53be88808c5221bd6ee0762e1af1def82b21d2061599b6cf5c79"));
     }
     
     @Test
     void assertDecryptWithoutSalt() {
         encryptAlgorithm.init(new Properties());
-        Object actual = encryptAlgorithm.decrypt("ab847c6f2f6a53be88808c5221bd6ee0762e1af1def82b21d2061599b6cf5c79", mock(EncryptContext.class));
+        Object actual = encryptAlgorithm.decrypt("ab847c6f2f6a53be88808c5221bd6ee0762e1af1def82b21d2061599b6cf5c79", mock(AlgorithmSQLContext.class));
         assertThat(actual.toString(), is("ab847c6f2f6a53be88808c5221bd6ee0762e1af1def82b21d2061599b6cf5c79"));
     }
     
     @Test
     void assertDecryptWithNullCiphertext() {
-        assertNull(encryptAlgorithm.decrypt(null, mock(EncryptContext.class)));
+        assertNull(encryptAlgorithm.decrypt(null, mock(AlgorithmSQLContext.class)));
     }
 }
